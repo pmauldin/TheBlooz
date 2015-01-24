25fbc5219beaf4c098caddcf849782551c00fdf7
Peter Mauldin <petermauldin@utexas.edu>
2015-01-24 00:00:30 -0600

    Working on Level1

---
+++ Unity/Assets/Scripts/SmoothFollow.js
@@ -0,0 +1,22 @@
+﻿var target : Transform;
+var distance = 8.5;
+var height = 3.0;
+var damping = 5.0;
+var xOffset = 5.0;
+
+function Update () {
+	wantedPosition = target.TransformPoint(0, height, -distance);
+	transform.position = wantedPosition;
+	transform.position.x += xOffset;
+
+	transform.LookAt (target, target.up);
+	
+	transform.localRotation = Quaternion.Euler(0, 0, 0);
+	
+}
+
+function LateUpdate()
+{
+   transform.position.y = 1.81;  
+	print(transform.position.x);
+}
