b6a6d4fce9c10b1beb4d4b1fed04b3a8feb8a9b7
Peter Mauldin <petermauldin@utexas.edu>
2015-01-24 18:29:23 -0600

    Working...

--- Unity/Assets/Scripts/PlayerLogic/FeetCollision.js
+++ Unity/Assets/Scripts/PlayerLogic/FeetCollision.js
@@ -10,7 +10,7 @@ function Update () {
 
 function OnCollisionEnter2D(coll: Collision2D) {
 	
-	if(coll.contacts[0].normal.x == 0 && (coll.gameObject.tag == "Floor" || coll.gameObject.tag == "Block")) {
+	if(coll.contacts[0].normal.x == 0 && coll.gameObject.tag.Contains("Ground")) {
 		SendMessageUpwards("setGrounded", coll);
 	}
 }
--- Unity/Assets/Scripts/PlayerLogic/Player2D.js
+++ Unity/Assets/Scripts/PlayerLogic/Player2D.js
@@ -2,6 +2,7 @@
 
 var X : float;
 var speed = 5.0;
+var finalSpeed = speed;
 var jumpSpeed = 10.0;
 var grounded = false;
 var smashing = false;
@@ -15,29 +16,37 @@ function Start () {
 
 function Update () {
 //	print(Input.GetAxis("Horizontal"));
+	// Moving
 	if(Mathf.Abs(Input.GetAxis("Horizontal")) >  0.05) {
+		if(!grounded) {
+			speed *= 1.5;
+		}
 		transform.position.x += Input.GetAxis("Horizontal") / speed;
 		if(target != null) {
 			target.transform.position.x += Input.GetAxis("Horizontal") / speed;
 		}
 	}
-			
+	
+	// Jumping		
 	if((Input.GetKeyDown("up") || Input.GetKeyDown("w")) && grounded) {
 		rigidbody2D.velocity.y = 0.0;
 		grounded = false;
 		rigidbody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
 	}
 	
+	// "Smashing"
 	if((Input.GetKeyDown("down") || Input.GetKeyDown("s")) && !smashing) {
 		smashing = true;
 		rigidbody2D.velocity.y = 0.0;
 		rigidbody2D.AddForce(new Vector2(0, -jumpSpeed), ForceMode2D.Impulse);
 	}
+	
+	speed = finalSpeed;
 	textureFlip();
 }
 
 function OnCollisionEnter2D(coll: Collision2D) {
-	if(coll.gameObject.tag == "Block" && coll.contacts[0].normal.x != 0) {
+	if(coll.gameObject.tag.Contains("Pushable") && coll.contacts[0].normal.x != 0) {
 		if(Mathf.Abs(transform.position.y - coll.gameObject.transform.position.y) < 1) {
 			target = coll.gameObject;
 		}
@@ -45,7 +54,7 @@ function OnCollisionEnter2D(coll: Collision2D) {
 }
 
 function OnCollisionExit2D(coll: Collision2D) {
-	if(coll.gameObject.tag == "Block" && target != null) {
+	if(coll.gameObject.tag.Contains("Pushable") && target != null) {
 		target = null;
 	}
 }
@@ -54,13 +63,13 @@ function setGrounded(coll: Collision2D) {
 	grounded = true;
 	smashing = false;
 }
-
-function springJump() {
-	if(!grounded) {
-		rigidbody2D.AddForce(new Vector2(0, jumpSpeed*1.5), ForceMode2D.Impulse);
-		grounded = false;
-	}
-}
+//
+//function springJump() {
+//	if(!grounded) {
+//		rigidbody2D.AddForce(new Vector2(0, jumpSpeed*1.5), ForceMode2D.Impulse);
+//		grounded = false;
+//	}
+//}
 
 function textureFlip() {
 	if(Input.GetAxis("Horizontal") >= 0.05){
